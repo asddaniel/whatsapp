ea3a3fdddb61c3206f6ba591d62105e5713ab873
Daniel Assani <devasddaniel@gmail.com>
2023-07-18 21:55:29 +0200

    data

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,8 +1,51 @@
 import React, {ReactNode} from "react";
 import rlogo from "../assets/react.svg";
 import filese from "../assets/file.jpg"
+import elvin from "../assets/elvin.jpg"
+import marden from "../assets/marden.jpg"
+import raissa from "../assets/raissa.jpg"
 
 export default class Conversation extends React.Component {
+    constructor(props: any){
+        super(props)
+        this.state = {
+            conversation: [{
+                name: "Elvin",
+                content:"bonjour", 
+                image:elvin, 
+                nombre:3, 
+                date: "19:53",
+            }, 
+            {
+                name: "Raissa",
+                content:"Comment tu va ? ", 
+                image:raissa,
+                nombre:2, 
+                date: "18:26",
+            }, 
+            {
+                name: "Marden",
+                content:"le programme est fini", 
+                image:marden, 
+                nombre:6, 
+                date: "18:03",
+            }, 
+            {
+                name: "CERCLE MATH",
+                content:"Elvin: Le lien vers...", 
+                image:filese,
+                nombre:7, 
+                date: "17:53",
+            }, 
+            {
+                name: "Joseph dev",
+                content:"bonjour, les props ...", 
+                image:rlogo, 
+                nombre:9, 
+                date: "16:38",
+            }]
+        }
+    }
     render(): ReactNode {
         return (
             <div className="container-fluid h-full bg-white p-1">
@@ -10,7 +53,7 @@ export default class Conversation extends React.Component {
                     <div className="col-lg-2 col-3 d-flex justify-content-center align-items-center p-2">
                          <img src={rlogo} alt="" className="img-fluid    aspect-carre round-carre" />
                     </div>
-                    <div className="col-7 col-lg-8">
+                    <div className="col-7 col-lg-8 d-flex flex-column justify-content-center">
                         <div className="row justify-content-start ">
                             <div className="fw-bold text-start h5">React dev</div>
                         </div>
@@ -20,42 +63,37 @@ export default class Conversation extends React.Component {
 
                     </div>
                     <div className="col-lg-2 col-2 p-1">
-                        <div className="row">
-                            <div className="fw-bold text-green text-center">20:53</div>
-                        </div>
-                        <div className="row p-1">
-                            <div className="d-flex justify-content-center align-items-center">
-                            <div className="fw-bold text-white bg-green rounded-circle aspect-carre p-2 px-3">2</div>
-                            </div>
-                           
-                        </div>
+                        
                     </div>
                 </div>
-                <div className="row p-2">
+                {this.state?.conversation?.map((conv: any) => (
+                    <div className="row p-2">
                     <div className="col-lg-2 col-3 d-flex justify-content-center p-2">
-                         <img src={filese} alt="" className="img-fluid    aspect-carre round-carre" />
+                         <img src={conv.image} alt="" className="img-fluid    aspect-carre round-carre" />
                     </div>
                     <div className="col-7 col-lg-8 d-flex flex-column justify-content-center">
                         <div className="row justify-content-start ">
-                            <div className="fw-bold text-start h5">Cercle MATH</div>
+                            <div className="fw-bold text-start h5">{conv.name}</div>
                         </div>
                         <div className="row justify-content-start ">
-                            <div className="fw-light text-start ">Bonjour trucmashing</div>
+                            <div className="fw-light text-start ">{conv.content}</div>
                         </div>
 
                     </div>
                     <div className="col-2 p-1 ">
                         <div className="row">
-                            <div className="fw-bold text-green text-center">20:53</div>
+                            <div className="fw-bold text-green text-center size-small">{conv.date}</div>
                         </div>
                         <div className="row p-1">
                             <div className="d-flex justify-content-center align-items-center ">
-                            <div className="fw-bold text-white bg-green rounded-circle aspect-carre p-status">8</div>
+                            <div className="fw-bold text-white bg-green rounded-circle aspect-carre p-status">{conv.nombre}</div>
                             </div>
                            
                         </div>
                     </div>
                 </div>
+                
+                ))}
                 
             </div>
         );
